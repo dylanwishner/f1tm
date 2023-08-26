2d3b2ceeb5e51ad4caa76dc36a0ed47e5163dacb
Dylan Wishner <dylan.wishner@gmail.com>
2023-08-25 22:48:08 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    let host = "192.168.56.1";
+    let port = 20777;
+    let address = format!("{host}:{port}");
+    let mut buffer = [0; 60577];
+    let socket = UdpSocket::bind(address)?;
+
+    loop {
+        let (_size, _addr) = socket.recv_from(&mut buffer)?;
+        println!("{:?}", &buffer);
+    }
+}
